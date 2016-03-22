aa3478d467b06e5c9c7c4c4d101c50254f1d3fcd
Laurie Linz <lvlinz@comcast.net>
2016-03-22 17:20:28 -0600

    Added some validations
    [#115830809]

--- app/models/student.rb
+++ app/models/student.rb
@@ -1,3 +1,6 @@
 class Student < ActiveRecord::Base
   validates :first_name, presence: true
+  validates :last_name, presence: true
+  validates :email, presence: true
+  validates :home_phone,:cell_phone, :work_phone, numericality: true
 end
